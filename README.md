37a59cf74411a78f3b77cba169da69dd89ebb4d2
Hannah Manfredi <hmanfredi@gmail.com>
2021-05-19 20:36:09 -0700

    Add Public directory and serve from index.js (express server). Still getting CORS issue which I can only resolve by each service fixing it own CORS errors.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # booking_proxy
-Proxy for Booking service. 
+Proxy for Booking service.
+
+INSTRUCTIONS:
+  git pull origin main for each service
+  all the npm installs
+  build their thing
+  npm start their thing
+  look at everyone's seeding script
+    make sure db is seeded
+  start my own proxy server
+
--- index.js
+++ index.js
@@ -1,45 +1,15 @@
 const express = require('express');
-const morgan = require("morgan");
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-// Configuration
 const PORT = 2000;
 const HOST = "localhost";
-const BOOKING_SERVICE_URL = "https://localhost:3002";
 
-app.use(morgan('dev'));
+app.use(express.static('public'));
 
-//test my Proxy
-app.get('/info', (req, res, next) => {
-  res.send('This is a test.');
-});
-
-// Authorization: to be included when I build out Auth
-// app.use('', (req, res, next) => {
-//   if (req.headers.authorization) {
-//       next();
-//   } else {
-//       res.sendStatus(403);
-//   }
-// });
-
-// Proxy endpoints
-app.use('/localhost:3002', createProxyMiddleware({
-  target: BOOKING_SERVICE_URL,
-  changeOrigin: true,
-  pathRewrite: {
-      ['/localhost:3002']: '',
-  },
-}));
-
-// Start the Proxy
+// Start my Proxy Server
 app.listen(PORT, HOST, () => {
   console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
-
-//test my proxy:
-//curl http://localhost:3002/booking?campId=0
 
 
