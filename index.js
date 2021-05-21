976741d75141a3a943f0438cb4bc89f2262e1802
Hannah Manfredi <hmanfredi@gmail.com>
2021-05-21 13:39:11 -0700

    Write 2nd proxy server which handles request from multiple origins
    i Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const { proxy } = require('http-proxy-middleware');
+const path = require('path');
+
+const PORT = process.env.PORT || 2000;
+const HOST = "localhost";
 
 const app = express();
 
-const PORT = 2000;
-const HOST = "localhost";
-
 app.use(express.static('public'));
+
+app.use('/proxy', proxy({
+  pathRewrite: {
+     '^/proxy/': '/'
+  },
+  target: 'https://localhost:2000',
+  secure: false
+}));
+
+app.get('*', (req, res) => {
+ res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 
 // Start my Proxy Server
 app.listen(PORT, HOST, () => {
-  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+  console.log(`Starting Proxy Server at ${HOST}:${PORT}`);
 });
 
-
+//add to my front-end:
+// const URL = `/proxy/${PATH}`;
+// return axios.get(URL);
