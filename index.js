1d933f2e750f4fb5e1f4dcc07208a2bcc5d0d554
Hannah Manfredi <hmanfredi@gmail.com>
2021-06-08 10:58:18 -0700

    Minor tweaks to my proxy deployment on s3

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 
 const proxy = express();
+proxy.use(cors());
+
 const mode = process.env.NODE_ENV;
 console.log(`hi you are in ${mode}`);
 
 let PORT = 0;
+let url = '';
 
-if (mode === 'development') {
+if (mode === 'development' || mode === undefined) {
   proxy.use(express.static(__dirname + '/public/dev'));
   PORT = 2000;
+  url += "http://localhost:2000";
 } else if (mode === 'production') {
   proxy.use(express.static(__dirname + '/public/prod'));
   PORT = 80;
+  url += "http://ec2-18-117-219-48.us-east-2.compute.amazonaws.com";
 }
 
-const dev_proxy = "http://localhost:2000";
-const prod_proxy = "http://ec2-3-15-24-53.us-east-2.compute.amazonaws.com";
-
 proxy.listen(PORT, () => {
-  console.log(`Starting Proxy Server at port ${PORT}`);
+  console.log(`Starting Proxy Server at port ${url}`);
 });
-
