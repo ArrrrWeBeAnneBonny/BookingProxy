7d29b4a3045ffd04f18c6d741b534641edd63f9e
Hannah Manfredi <hmanfredi@gmail.com>
2021-06-16 12:38:30 -0700

    Remove \

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const allowedOrigins = [
   'http://localhost:2000/',
   'http://localhost:3002/',
   "https://fec-booking.s3.us-west-1.amazonaws.com/1623867148511.booking.js",
-  'http://ec2-54-151-15-127.us-west-1.compute.amazonaws.com/',
+  'http://ec2-54-151-15-127.us-west-1.compute.amazonaws.com',
   'http://67.160.218.95.32:3002/',
   'http://67.160.218.95.32:80/',
   'http://67.160.218.95.32/'
@@ -18,7 +18,7 @@ const allowedOrigins = [
 proxy.use(cors({ origin: allowedOrigins }));
 
 const PORT = 80;
-const url = 'http://ec2-54-151-15-127.us-west-1.compute.amazonaws.com/';
+const url = 'http://ec2-54-151-15-127.us-west-1.compute.amazonaws.com';
 
 proxy.use(express.static('dist'));
 
