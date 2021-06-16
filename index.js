001d967c2231215a91aaa0b7d06a5b25c6b11a74
Hannah Manfredi <hmanfredi@gmail.com>
2021-06-16 13:51:43 -0700

    Delete two more /

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const allowedOrigins = [
   "https://fec-booking.s3.us-west-1.amazonaws.com/styles.css",
   'http://ec2-54-151-15-127.us-west-1.compute.amazonaws.com',
   'http://ec2-13-56-114-222.us-west-1.compute.amazonaws.com',
-  'http://67.160.218.95.32:3002/',
-  'http://67.160.218.95.32:80/',
-  'http://67.160.218.95.32/'
+  'http://67.160.218.95.32:3002',
+  'http://67.160.218.95.32:80',
+  'http://67.160.218.95.32'
 ];
 
 proxy.use(cors({ origin: allowedOrigins }));
