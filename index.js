fba36b414481c4427d95757dd9e0dd67d6ec5066
Hannah Manfredi <hmanfredi@gmail.com>
2021-06-16 13:20:51 -0700

    add another ec2 in allowed origins

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const allowedOrigins = [
   "https://fec-booking.s3.us-west-1.amazonaws.com/booking.js",
   "https://fec-booking.s3.us-west-1.amazonaws.com/styles.css",
   'http://ec2-54-151-15-127.us-west-1.compute.amazonaws.com',
+  'http://ec2-13-56-114-222.us-west-1.compute.amazonaws.com/',
   'http://67.160.218.95.32:3002/',
   'http://67.160.218.95.32:80/',
   'http://67.160.218.95.32/'
